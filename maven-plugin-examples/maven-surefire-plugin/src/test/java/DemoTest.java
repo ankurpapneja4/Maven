912629cfcc6731a335894266385f03f57b597b68
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-11-25 18:06:27 +0530

    Plugin - maven-surefire-plugin - Run Pojo Test. Closes #4

---
+++ maven-plugin-examples/maven-surefire-plugin/src/test/java/DemoTest.java
@@ -0,0 +1,10 @@
+public class DemoTest {
+
+    public void testHelloWorld() {
+        System.out.println("Test - Hello World - Running");
+    }
+
+    public void testFooBar() {
+        System.out.println("Test - Foo Bar - Running");
+    }
+}
