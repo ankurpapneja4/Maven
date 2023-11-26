4963bea74345b3e05f47570b6bc75291c6f87477
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-11-26 18:51:07 +0530

    Add Dependency For Other Models Within Project. For #12

---
+++ multi-module-example/web-app/src/main/java/com/example/webapp/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.webapp.controller;
+
+import com.example.entities.User;
+import com.example.domain.UserDTO;
+
+public class UserController {
+
+    public User saveUser(UserDTO userDTO){
+        return new User();
+    }
+}
